c3b9ea932f22cb62135246c37b542c074759000d
Ramon van Alteren <ramon@vanalteren.nl>
2015-09-13 23:09:02 +0200

    Added mount/format commands for the pdisk
    Added attempt (untested) at auto-formatting and mounting the pdisk

--- scripts/create_server_instance.sh
+++ scripts/create_server_instance.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash 
+#!/usr/bin/ bash 
 
 # Create an instance in the zone specified.
 # Create a persistent disk for saving world-data
@@ -6,7 +6,7 @@
 
 source vars.sh
 
-gcloud compute disks create ${HOSTNAME}-world-disk1 --zone ${ZONE}
+gcloud compute disks create ${DISK_NAME} --zone ${ZONE}
 
 gcloud compute addresses create ${HOSTNAME}-ext-ip --region ${REGION}
 
@@ -15,7 +15,7 @@ gcloud compute instances create ${HOSTNAME} \
     --zone ${ZONE} \
     --machine-type ${MACHINE_TYPE} \
     --address ${HOSTNAME}-ext-ip \
-    --disk name=${HOSTNAME}-world-disk1 \
+    --disk name=${DISK_NAME} \
     --scopes storage-rw,compute-rw
 
 # gcloud compute instances attach-disk ${HOSTNAME} \
@@ -24,3 +24,6 @@ gcloud compute instances create ${HOSTNAME} \
 #    --zone ${ZONE}
 
 gcloud compute ssh ${HOSTNAME} --zone ${ZONE} "echo \"mounting disk ${HOSTNAME}-world-disk1\""
+gcloud compute ssh ${HOSTNAME} --zone ${ZONE} "sudo mkdir -p ${MOUNT_POINT}"
+gcloud compuet ssh ${HOSTNAME} --zone ${ZONE} "sudo /usr/share/google/safe_format_and_mount -m \"mkfs.ext4 -F\" \
+    /dev/disk/by-id/google-${DISK_NAME} ${MOUNT_POINT}
